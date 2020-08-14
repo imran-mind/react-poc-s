4e69c1930fc05b1e0d9eb39b7edcd841ba254af4
Shaikh Imran <shaikhimran115@gmail.com>
2020-08-14 20:08:26 +0530

    Update spotify.js

--- spotify-clone/src/spotify.js
+++ spotify-clone/src/spotify.js
@@ -1,5 +1,4 @@
 
-// const cliendSecret='339ab5e8d9c8410b8b7786519d2cd351';
 
 export const authEndpoint="https://accounts.spotify.com/authorize";
 
@@ -7,7 +6,6 @@ export const authEndpoint="https://accounts.spotify.com/authorize";
 
 const redirectUri = "https://spotify-clone-12fd3.web.app/";
 
-const cliendId='292e432d261c467eada43c7cec2e4587';
 
 const scopes =[
     "user-read-currently-playing",
